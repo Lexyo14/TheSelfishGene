f9f515ecda9fc20d5f2cf5e0f792fbde31a90bea
tom837 <71010075+tom837@users.noreply.github.com>
2022-06-17 16:36:41 +0200

    Update Philanderers.java

--- src/Philanderers.java
+++ src/Philanderers.java
@@ -1,15 +1,8 @@
 public class Philanderers extends Men implements Runnable {
-    private static int cntp = 0;
     public int num;
     private int health=0;
-    Philanderers (){
-        cntp++;
-        super.cntm++;
-    }
 
-    public int countp(){
-        return cntp;
-    }
+
     public void ChangeHealth(int k){
         health+=k;
     }
