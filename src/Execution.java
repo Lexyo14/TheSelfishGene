6f6344dbb6fd612277f40a59aae79276def70762
tom837 <71010075+tom837@users.noreply.github.com>
2022-06-17 16:39:48 +0200

    Update Execution.java

--- src/Execution.java
+++ src/Execution.java
@@ -2,7 +2,7 @@ public class Execution {
 
     public static void main(String[] args) {
         Battle b = new Battle();
-        b.exe(5,9,2,5,5,5,5, 40);
+        System.out.println(b.exe(5,9,2,5,5,5,5, 30,10,-30,-30,10,7,10));
 
 
 
