31665b2bdc26bbd8f67a49418023e9352cee16fa
tom837 <71010075+tom837@users.noreply.github.com>
2022-06-20 13:55:07 +0200

    Update Women.java

--- src/main/java/TheSelfishGene/Women.java
+++ src/main/java/TheSelfishGene/Women.java
@@ -2,6 +2,8 @@ package TheSelfishGene;
 
 public interface Women {
 
+    //just to not create an error when running 
+    //public int get_exp(){return 5;}
     public int Id();
 
     public void ChangeHealth(int k);
