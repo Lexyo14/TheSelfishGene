46f6f0051dd7d42fbd6ebb6122df40ebd9803f65
tom837 <71010075+tom837@users.noreply.github.com>
2022-06-20 13:45:36 +0200

    Update Club.java

--- src/main/java/TheSelfishGene/Club.java
+++ src/main/java/TheSelfishGene/Club.java
@@ -10,8 +10,14 @@ public class Club extends Battle {
     public void meet() {
         //Main function of CLUb()
         //makes people copulate
-
-        //System.out.println(women_l.size());
+/*
+        int ill=(int)Math.floor(Math.random()*(100-0+1)+0);
+        if (ill<= bf.get_exp()){
+            //if ill is <= expactancy of the male kill him and end the thread
+            tmpm.add(bf);
+            return;
+        }
+        */
         Women gf;
         synchronized (women_l) {
             if (maxw == 0) {
@@ -28,7 +34,18 @@ public class Club extends Battle {
             gf = women_l.remove(random_girl);
             //gf = curren Woman
             maxw--;
-            women_l.add(gf);//wtf
+            women_l.add(gf);
+/*           
+            //same as for the man
+            ill=(int)Math.floor(Math.random()*(100-0+1)+0);
+            if (ill<= gf.get_exp()){
+                tmpw.add(gf);
+                return;
+                */
+
+        }
+            
+            
         }
 
         int h1 = 0;
@@ -39,6 +56,12 @@ public class Club extends Battle {
 
         //is the child's gonna have a mutation?
         int mutate = (int) Math.floor(Math.random() * (100 - 0 + 1) + 0);//if this number less than mutation, it mutates
+        
+        /*
+        getting the maximum and minimum of the expactancy of the father and mother
+        int max = Math.max(bf.get_exp(),gf.get_exp() );
+        int min = Math.min(bf.get_exp(),gf.get_exp());
+        */
 
         //depending on the matching:
         //Each type of people has it's unique id which sum will give a new id.
@@ -59,10 +82,14 @@ public class Club extends Battle {
                         if (mutate<=mut) {
                             cntc++; //the child is a different from mother
                             Coy girl = new Coy();
+                            // for random death delete line above and uncomment line below
+                            //Coy girl = new Coy((int)Math.floor(Math.random()*(max-min+1)+min));
                             women_l.add(girl);
                         } else {    //same type of mother
                             cnts++;
                             Fast girl = new Fast();
+                            // for random death delete line above and uncomment line below
+                            //Coy Fast = new Fast((int)Math.floor(Math.random()*(max-min+1)+min));
                             women_l.add(girl);
                         }
                         break;
@@ -70,10 +97,14 @@ public class Club extends Battle {
                         if (mutate<=mut) {
                             cntf++;//the child is a different from father
                             Faithfull boy = new Faithfull();
+                            // for random death delete line above and uncomment line below
+                            //Faithfull girl = new Faithfull((int)Math.floor(Math.random()*(max-min+1)+min));
                             men_l.add(boy);
                         } else {    //same type of father
                             cntp++;
                             Philanderers boy = new Philanderers();
+                            // for random death delete line above and uncomment line below
+                            //Philandereres girl = new Philanderers((int)Math.floor(Math.random()*(max-min+1)+min));
                             men_l.add(boy);
                         }
                         break;
@@ -90,10 +121,14 @@ public class Club extends Battle {
                         if (mutate<=mut) {
                             cnts++;
                             Fast girl = new Fast();
+                            // for random death delete line above and uncomment line below
+                            //Fast girl = new Fast((int)Math.floor(Math.random()*(max-min+1)+min));
                             women_l.add(girl);
                         } else {
                             cntc++;
                             Coy girl = new Coy();
+                            // for random death delete line above and uncomment line below
+                            //Coy girl = new Coy((int)Math.floor(Math.random()*(max-min+1)+min));
                             women_l.add(girl);
                         }
                         break;
@@ -101,10 +136,14 @@ public class Club extends Battle {
                         if (mutate<=mut) {
                             cntp++;
                             Philanderers boy = new Philanderers();
+                            // for random death delete line above and uncomment line below
+                            //Philanderers girl = new Philanderers((int)Math.floor(Math.random()*(max-min+1)+min));
                             men_l.add(boy);
                         } else {
                             cntf++;
                             Faithfull boy = new Faithfull();
+                            // for random death delete line above and uncomment line below
+                            //Faithfull girl = new Faithfull((int)Math.floor(Math.random()*(max-min+1)+min));
                             men_l.add(boy);
                         }
                         break;
@@ -120,10 +159,14 @@ public class Club extends Battle {
                         if (mutate<=mut) {
                             cntc++;
                             Coy girl = new Coy();
+                            // for random death delete line above and uncomment line below
+                            //Coy girl = new Coy((int)Math.floor(Math.random()*(max-min+1)+min));
                             women_l.add(girl);
                         } else {
                             cnts++;
                             Fast girl = new Fast();
+                            // for random death delete line above and uncomment line below
+                            //Fast girl = new Fast((int)Math.floor(Math.random()*(max-min+1)+min));
                             women_l.add(girl);
                         }
                         break;
@@ -131,10 +174,14 @@ public class Club extends Battle {
                         if (mutate<=mut) {
                             cntp++;
                             Philanderers boy = new Philanderers();
+                            // for random death delete line above and uncomment line below
+                            //Philanderers girl = new Philanderers((int)Math.floor(Math.random()*(max-min+1)+min));
                             men_l.add(boy);
                         } else {
                             cntf++;
                             Faithfull boy = new Faithfull();
+                            // for random death delete line above and uncomment line below
+                            //Faithfull girl = new Faithfull((int)Math.floor(Math.random()*(max-min+1)+min));
                             men_l.add(boy);
                         }
                         break;
