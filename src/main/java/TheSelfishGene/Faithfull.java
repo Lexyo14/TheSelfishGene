5d6cc688171e82e9c26dcb53805961801c76977a
tom837 <71010075+tom837@users.noreply.github.com>
2022-06-20 13:49:34 +0200

    Update Faithfull.java

--- src/main/java/TheSelfishGene/Faithfull.java
+++ src/main/java/TheSelfishGene/Faithfull.java
@@ -3,6 +3,18 @@ package TheSelfishGene;
 public class Faithfull implements Men {
     public int num;
     private int health=0;
+    /*
+    // exp is a variable for random death
+    public int exp;
+    Faithfull(int exp){
+        this.exp=exp;
+    }
+
+    public int get_exp(){
+        return this.exp;
+    }
+    */
+    
     public int Id(){
         return 6;
     }
